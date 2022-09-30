f1fe985b228d7c479aa700c1aba4fb9a85cdee90
AgusKorstanje <agus.korstanje@gmail.com>
2022-09-30 00:24:47 -0300

    v.2

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 let formulario = document.getElementById("formulario");
-const div = document.getElementById("div");
+let div = document.getElementById("div");
 const personas = [];
 let contador = 0;
 
 class Persona{
-    constructor (info){
-        this.nombre = info.nombre,
-        this.turno = info.turno
+    constructor (persona){
+        this.nombre = persona.nombre,
+        this.turno = persona.turno
     }
 }
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
     let inputs = e.target.children;
-    /* let nombre = inputs[1].value; */
     personas.push(new Persona({
         nombre : inputs[1].value,
         turno : contador + 1
     }
     ))
-    contador ;
-    console.log(personas);
+    contador ++;
+    personas.forEach(persona => {
+        let turnopantalla = document.createElement("div")
+        turnopantalla.innerHTML = `
+        <h2>Nombre: ${persona.nombre}</h2>
+        <h3>Turno: ${persona.turno}</h3>`
+        div.append(turnopantalla);
+    })
 })
 
-personas.forEach(persona => {
-    let turnopantalla = document.createElement("div")
-    turnopantalla.innerHTML = `
-    <h2>Nombre: ${persona.nombre}</h2>
-    <h3>Turno: ${persona.turno}</h3>`
-    div.append(turnopantalla);
-})
